7c8fa0dca71f1d9db85134580a38c984b1479085
Sekou <sekouk1410@gmail.com>
2025-10-14 11:57:18 +0000

    Commit pour connecter le depot local et distant

---
+++ src/main/java/com/ngaso/Ngaso/NgasoApplication.java
@@ -0,0 +1,13 @@
+package com.ngaso.Ngaso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NgasoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NgasoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ngaso/Ngaso/NgasoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ngaso.Ngaso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NgasoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
