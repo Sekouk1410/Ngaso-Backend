b404c710fec9398e44f9fbd7fb0deaff55bbe982
Sekou <sekouk1410@gmail.com>
2025-10-15 23:19:39 +0000

    Ajout de la creation d'admin automatique au demarrage

--- src/main/java/com/ngaso/Ngaso/DAO/AdministrateurRepository.java
+++ src/main/java/com/ngaso/Ngaso/DAO/AdministrateurRepository.java
@@ -2,5 +2,9 @@ package com.ngaso.Ngaso.DAO;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.ngaso.Ngaso.Models.entites.Administrateur;
+import org.springframework.stereotype.Repository;
 
-public interface AdministrateurRepository extends JpaRepository<Administrateur, Integer> {}
+@Repository
+public interface AdministrateurRepository extends JpaRepository<Administrateur, Integer> {
+    boolean existsByEmail(String email);
+}
